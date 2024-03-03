1823051af93c1777e996dd036250266277a941c5
ayush-maurya-19 <ayushmaurya1901@gmail.com>
2024-03-03 13:48:07 +0530

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import styles from "./homepage.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <Link href="/">
+        <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</h1>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime,
+        consequuntur error repudiandae nostrum dicta aut recusandae eveniet id
+        in temporibus asperiores aliquam odio eos est ab voluptatibus fuga
+        laudantium dolore.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime,
+        consequuntur error repudiandae nostrum dicta aut recusandae eveniet id
+        in temporibus asperiores aliquam odio eos est ab voluptatibus fuga
+        laudantium dolore.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime,
+        consequuntur error repudiandae nostrum dicta aut recusandae eveniet id
+        in temporibus asperiores aliquam odio eos est ab voluptatibus fuga
+        laudantium dolore.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime,
+        consequuntur error repudiandae nostrum dicta aut recusandae eveniet id
+        in temporibus asperiores aliquam odio eos est ab voluptatibus fuga
+        laudantium dolore.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime,
+        consequuntur error repudiandae nostrum dicta aut recusandae eveniet id
+        in temporibus asperiores aliquam odio eos est ab voluptatibus fuga
+        laudantium dolore.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime,
+        consequuntur error repudiandae nostrum dicta aut recusandae eveniet id
+        in temporibus asperiores aliquam odio eos est ab voluptatibus fuga
+        laudantium dolore.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime,
+        consequuntur error repudiandae nostrum dicta aut recusandae eveniet id
+        in temporibus asperiores aliquam odio eos est ab voluptatibus fuga
+        laudantium dolore.
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Maxime,
+        consequuntur error repudiandae nostrum dicta aut recusandae eveniet id
+        in temporibus asperiores aliquam odio eos est ab voluptatibus fuga
+        laudantium dolore.
+      </Link>
+    </div>
+  );
+}
